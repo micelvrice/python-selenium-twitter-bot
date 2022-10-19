4d81065e7dc9b8fb7510d2a869b5845c8fa8db61
yeserumo <49807842+yeserumo@users.noreply.github.com>
2022-10-19 17:39:18 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
-from TwitterUser import *
+import os, time
+from twitter_bot_class import TwitterBot
+import configparser
+from utils import *
 
-test = TwitterUser('realcaixia', ['myspeak', 'test'],'beijing')
-print(test.userTweets)
+
+
+if __name__ == "__main__":
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.read('config.conf')
+
+    email = config.get('account', 'email')
+    password = config.get('account', 'password')
+    userName = config.get('account', 'userName')
+    searchKeys = config.get('search', 'searchKeys')
+    firstLevelUserTweetsFile = config.get('output', 'outputTweetsPath')
+    secondLevelUserTweetsFile = config.get('output', 'secondLevelUserTweetsFile')
+    maxScrollDownTimes = config.get('settings', 'maxScrollDownTimes')
+    firstLevelUsersFile = config.get('data', 'firstLevelUserFile')
+    userId = config.get('search', 'userId')
+    urlPrefix = config.get('settings', 'urlPrefix')
+    translateTargetLanguage = config.get('settings', 'translateTargetLanguage')
+
+
+    tb = TwitterBot(email, password, userName, maxScrollDownTimes, firstLevelUserTweetsFile, secondLevelUserTweetsFile, urlPrefix, translateTargetLanguage)
+    tb.loginAbnormal()
+
+    # 获得一级用户列表['dawn_wei_tan', 'dansoncj', ...]
+    firstLevelUsers = readCSVToGetTwitterUserLink(firstLevelUsersFile)
+
+    # 获取一级用户主页的所有Tweets并将tweets写入外部文件
+    firstLevelTweets = tb.getAndWriteUserTweets(firstLevelUsers, firstLevelUserTweetsFile)
+    # time.sleep(5)
+
+    # 获得二级用户列表
+    secondLevelUsers = tb.getSecondLevelUsers(firstLevelUsers)
+
+    # 获取二级用户主页所有Tweets并将tweets写入外部文件
+    secondLevelTweets = tb.getAndWriteUserTweets(secondLevelUsers, secondLevelUserTweetsFile)
